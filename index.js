c2f914c3520663ba085821b42262b286c8d848c4
Anthony Mugendi <ngurumugz@gmail.com>
2021-04-25 19:26:53 +0300

    Added isSuspended

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const ntsuspend = require('ntsuspend');
 
-function suspend(process) {
+
+function suspend(process, updateIsSuspended) {
     if (isWindows()) {
         ntsuspend.suspend(process.pid);
+
     } else {
         process.kill('SIGSTOP');
     }
+    // indicate that process is suspended
+    if (updateIsSuspended) process.isSuspended = true;
 }
 
-function resume(process) {
+function resume(process, updateIsSuspended) {
     if (isWindows()) {
         ntsuspend.resume(process.pid);
     } else {
         process.kill('SIGCONT');
     }
+    // indicate that process is not suspended
+    if (updateIsSuspended) process.isSuspended = false;
 }
 
 function bind(process) {
-    process.suspend = () => suspend(process)
-    process.resume = () => resume(process)
+    process.suspend = () => suspend(process, true)
+    process.resume = () => resume(process, true)
+    process.isSuspended = false;
 }
 
 
